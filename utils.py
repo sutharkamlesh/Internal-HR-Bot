774aaf6020c6f1b55d1060cda943cba039be2241
Kamlesh Suthar <kamlesh08@live.com>
2019-08-13 12:40:03 +0530

    Code Rearrangement

--- utils.py
+++ utils.py
@@ -1,13 +1,5 @@
 import smtplib
 
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-
-# start TLS for security
-s.starttls()
-
-# Authentication
-s.login("kamlesh.suthar@techmatters.com", "fall0_40c")
 months = "January,February,March,April,May,June,July,August,September,October,November,December".split(",")
 
 
@@ -19,13 +11,21 @@ def date2text(date_string):
 
 # Creating a Function to send a mail
 def send_mail(to_email, subject, body):
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login("kamlesh.suthar@techmatters.com", "fall0_40c")
     message = """\
-            From: kamlesh.suthar@techmatters.com
-            To: {0}
-            Subject: {1} 
-            
-            {2}
-            """.format(to_email, subject, body)
+    From: kamlesh.suthar@techmatters.com
+    To: {0}
+    Subject: {1} 
+    
+    {2}
+    """.format(to_email, subject, body)
     # sending the mail
     s.sendmail("kamlesh.suthar@techmatters.com", to_email, message)
     # terminating the session
