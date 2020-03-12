62fcea7f4c241448223d9083420f0e8c08c0fce6
Keshav kumar <keshavrathor1998@gmail.com>
2020-03-12 11:54:15 +0530

    new update

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import smtplib
-import math, random
 
 months = "January,February,March,April,May,June,July,August,September,October,November,December".split(",")
 
@@ -19,8 +18,8 @@ def send_mail(to_email, subject, body):
     s.starttls()
 
     # Authentication
-    s.login("kamlesh.suthar@techmatters.com", "techmatters123")
-    message = """\\nFrom: kamlesh.suthar@techmatters.com\nTo: {0}\nSubject: {1}\n\n{2}""".format(to_email, subject, body)
+    s.login("digital@techmatters.com", "techmatters123")
+    message = """\\nFrom: digital@techmatters.com\nTo: {0}\nSubject: {1}\n\n{2}""".format(to_email, subject, body)
     # sending the mail
     s.sendmail("kamlesh.suthar@techmatters.com", to_email, message)
     # terminating the session
@@ -34,14 +33,3 @@ def batch_update_entities(project_id, entity_type, entities):
     parent = client.entity_type_path(project_id, entity_type)
     response = client.batch_create_entities(parent, entities)
     print("Entities Updated: \n{}".format(response))
-
-
-def generateOTP():
-    # Declare a string variable
-    # which stores all string
-    string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    OTP = ""
-    length = len(string)
-    for i in range(6):
-        OTP += string[math.floor(random.random() * length)]
-    return OTP
